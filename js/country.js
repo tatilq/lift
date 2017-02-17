19265cc38bc371c5100b5dad4c84081637caf4b9
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-02-17 10:23:07 -0500

    Optimizacion de codigo

--- js/country.js
+++ js/country.js
@@ -3,18 +3,21 @@ function init()
 {
 	var lista_paises=document.getElementById("lista_paises");
 	//-----------------------------------------------LISTA D EPAISES JSON------------------------------------------//
-	var paises='[{"nombre":"Australia","codigo": "61" , "img":"img/australia.png"},{"nombre":"Canada","codigo": "1" ,"img":"img/canada.png"},'+
-	    '{"nombre":"China","codigo": "86" , "img":"img/china.png"},{"nombre":"Francia","codigo": "33" , "img":"img/francia.jpg"},'+
-	    '{"nombre":"India","codigo": "91","img":"img/india.gif"},{"nombre":"Japon","codigo": "81","img":"img/japan.gif"},'+
-	    '{"nombre":"Mexico","codigo": "52","img":"img/mexico.png"},{"nombre":"Puerto Rico","codigo": "1","img":"img/puerto_rico.png"},'+
-	    '{"nombre":"Peru","codigo": "51","img":"img/peru.png"},{"nombre":"Estados Unidos","codigo":"1","img":"img/EeUu.gif"}]';
+	var paises='[{"nombre": "Afghanistan", "codigo": "+93","img":"img/BanderaDeAfganistan.png"},{"nombre": "Argentina","codigo": "+54","img":"img/argentina.jpg"},{"nombre":"Australia","codigo": "+61" , "img":"img/australia.png"},'+
+	'{"nombre": "Bolivia", "codigo": "+591","img":"img/bolivia.png"},{"nombre": "Brasil","codigo": "+55","img":"img/brasil.jpg"},{"nombre":"Canada","codigo": "+1" ,"img":"img/canada.png"},'+
+	'{"nombre":"China","codigo": "+86" , "img":"img/china.png"}, {"nombre": "Chile","codigo": "+56","img":"img/chile.png"},{"nombre":"Estados Unidos","codigo":"+1","img":"img/EeUu.gif"},{"nombre":"Francia","codigo": "+33" , "img":"img/francia.jpg"},'+
+	    '{"nombre": "Honduras","codigo": "+504","img":"img/honduras.jpeg"},{"nombre":"India","codigo": "+91","img":"img/india.gif"},{"nombre": "Irlanda","codigo": "+353","img":"img/irlanda.jpg"},{"nombre":"Japon","codigo": "+81","img":"img/japan.gif"},'+
+	    '{"nombre":"Mexico","codigo": "+52","img":"img/mexico.png"},{"nombre":"Puerto Rico","codigo": "+1","img":"img/puerto_rico.png"},'+
+	    '{"nombre":"Peru","codigo": "+51","img":"img/peru.png"},{"nombre": "Polonia","codigo": "+48","img":"img/polonia.jpg"},{"nombre": "Rusia","codigo": "+7","img":"img/rusia.png"},'+
+  		'{"nombre": "Singapur","codigo": "+65","img":"img/singapur.png"},{"nombre": "Suiza","codigo": "+41","img":"img/zuisa.jpg"},{"nombre": "Tailandia","codigo": "+66","img":"img/tailandia.gif"},{"nombre": "Taiwán","codigo": "+886","img":"img/taiwan.png"},'+
+  		'{"nombre": "Venezuela", "codigo": "+58","img":"img/mexico.png"}]';
 
 	var pais =JSON.parse(paises);
 	var longi=pais.length;
 
   	for(var i=0;i<longi;i++)
   	{
-  		var html='<a href="sign-up.html"><li select=""><img src='+pais[i].img + 
+  		var html='<a href="sign-up.html"><li class="lisPais"><img src='+pais[i].img + 
   		' class="img_responsive bandera"><span class="code"'+ 
   		' style="display:none;">'+pais[i].codigo+'</span>'+ pais[i].nombre+'</li></a>';
 
@@ -36,8 +39,21 @@ function onLinkClick(evt)
 	localStorage.setItem('codigo',codePais);
 }
 /****************************************************************FIN**************************************************/
-
-
-
+function searchChat()
+{
+	var chatsList=document.getElementsByClassName("lisPais");
+	var search = document.getElementById("search");//id del input
+	var forEach = Array.prototype.forEach;
+	search.addEventListener("keyup", function(e){
+	    var choice = this.value;
+	  
+	    forEach.call(chatsList, function(f){
+	        if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1 )
+	            f.style.display = "none";        
+	        else
+	            f.style.display = "block";        
+	    });
+	}, false);
+};
 
 
